a877fbad4fb0974d5812474001f003d102ea933e
vidyashreeathulkarkera-vak <vidyashreeathulkarkera@gmail.com>
2025-09-24 00:54:31 +0530

    finalCommit

--- gallery.js
+++ gallery.js
@@ -1,5 +1,4 @@
-var artworks = [
-  {
+var artworks=[   {
     description: "A lake in Norway in autumn.",
     address: "https://picsum.photos/id/123/400"
   },
@@ -15,30 +14,33 @@ var artworks = [
     description: "Happiness and peace",
     address: "https://picsum.photos/id/635/400"
   },
+   {
+    description: "Togetherness",
+    address: "togetherness.JPG"
+  }
 ]
-
 var feed = document.getElementById('art-feed');
-
-for (var i = 0; i < artworks.length; i++) {
+for(var i=0;i<artworks.length;i++){
   var feedBox = document.createElement('div');
   feedBox.setAttribute('class', 'feed-box');
 
   var feedImage = document.createElement('div');
   feedImage.setAttribute('class', 'feed-image');
 
-  var img = document.createElement('img');
+   var img = document.createElement('img');
   img.setAttribute('src', artworks[i].address);
 
-  var feedDescription = document.createElement('div');
+  var feedDescription=document.createElement('div');
   feedDescription.setAttribute('class', 'feed-description');
 
-  var text = document.createTextNode(artworks[i].description)
+  var text=document.createTextNode(artworks[i].description)
 
-  feedImage.appendChild(img);
-  feedDescription.appendChild(text);
-
-  feedBox.appendChild(feedImage);
-  feedBox.appendChild(feedDescription);
-
-  feed.appendChild(feedBox);
+   feedImage.appendChild(img);
+     feedDescription.appendChild(text);
+    feedBox.appendChild(feedImage);
+    feedBox.appendChild(feedDescription)
+     feed.appendChild(feedBox);
 }
+
+
+     
