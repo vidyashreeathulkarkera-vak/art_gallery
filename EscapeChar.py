f2cd659fc10d3b45ba6cae5f399301f2caf53a2c
vidyashreeathulkarkera-vak <vidyashreeathulkarkera@gmail.com>
2025-09-24 17:56:47 +0530

    python1

---
+++ EscapeChar.py
@@ -0,0 +1,19 @@
+splitString="This string has been\nsplit over\nseveral\nlines"
+print(splitString)
+
+tabbedString="1\t2\t3\t4\t5"
+
+print(tabbedString)
+
+print('The pet shop owner said "No,no,\'e\'s uh.....he\'s resting".')
+
+print("The pet shop owner said \"NO,no,'e's uh...resting\".")
+
+print("""The pet shop owner said "No,no,no, e's uh......resting".""")
+
+anotherSplitString="""This string beendasszs
+split over
+several
+lines"""
+
+print(anotherSplitString)
