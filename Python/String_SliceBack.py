37b4d032248cdb1f9ed924aee797b9f28a8b29b8
vidyashreeathulkarkera-vak <vidyashreeathulkarkera@gmail.com>
2025-09-25 17:28:54 +0530

    String

---
+++ Python/String_SliceBack.py
@@ -0,0 +1,21 @@
+letters="abcdefghijklmnopqrstuvwxyz"
+#backwards=letters[25:0:-1]
+#print(backwards) #zyxwvutsrqponmlkjihgfedcba
+
+#backwards=letters[::-1]
+#print(backwards) #zyxwvutsrqponmlkjihgfedcba
+
+
+backwards=letters[16:13:-1]
+print(backwards)
+
+backwards=letters[4::-1]
+print(backwards)
+
+backwards=letters[25:-9:-1]
+
+print(letters[-4:])
+print(letters[-1:])
+print(letters[:1])
+print(letters[0])
+
