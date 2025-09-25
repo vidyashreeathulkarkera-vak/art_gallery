35c6fe083e4a43f1e159cc7de921d65f8ec236f5
vidyashreeathulkarkera-vak <vidyashreeathulkarkera@gmail.com>
2025-09-25 16:13:14 +0530

    StringsAdded

---
+++ Python/String_NegativeIndexing.py
@@ -0,0 +1,10 @@
+parrot="Norwegien Blue"
+
+print(parrot[-11])
+print(parrot[-10]) 
+print()
+print(parrot[-11])  # 'u'
+print(parrot[-8]) # 'N'
+print(parrot[-6]) # 'N'
+
+# 'Norweg'
---
+++ Python/String_slicing.py
@@ -0,0 +1,16 @@
+parrot="Norwegian Blue"
+print(parrot[0:6]) #Norweg
+print(parrot[3:5]) #we 
+print(parrot[0:9]) #Norwegian
+print(parrot[0:9]) #Norwegian
+print(parrot[10:])
+
+print(parrot[:])
+
+
+letter="abcdefghijklmnopqrstuvwxyz"
+print(letter[:]) #prints all the letters
+print(letter[0:]) #prints all the letters
+print(letter[:26]) #prints all the letters
+print(letter[0:25]) #prints all the letters except z
+print(letter[0:25:3]) #prints all the letters except z and skips one letter
