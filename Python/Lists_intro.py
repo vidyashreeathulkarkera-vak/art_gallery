95ed8a1ea8e70c4b6aa634a987f97b43c03da05e
vidyashreeathulkarkera-vak <vidyashreeathulkarkera@gmail.com>
2025-09-30 20:30:05 +0530

    sep30

---
+++ Python/Lists_intro.py
@@ -0,0 +1,8 @@
+computer_parts=["computer","monitor","keyboard","mouse","mouse mat"]
+for part in computer_parts:
+    print("{}".format(part))
+print()
+print(computer_parts[0])
+
+print(computer_parts[0:3])
+print(computer_parts[-1])
---
+++ Python/immutable.py
@@ -0,0 +1,4 @@
+result=True
+another_result=result
+print(id(result))
+print(id(another_result))
